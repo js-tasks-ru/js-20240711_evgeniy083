f58c13ed57582254e3cb72f017240c07995cc83f
Eugene Yakunichev <ev.iakunichev@stalcom.com>
2024-07-15 16:55:56 +0300

    Настройка задачника

--- 01-intro/1-sum/index.test.js
+++ 01-intro/1-sum/index.test.js
@@ -4,4 +4,11 @@ describe('intro/sum', () => {
   it('should return sum of numbers', () => {
     expect(sum(1, 1)).toEqual(2);
   });
+  it('should return sum of numbers', () => {
+    expect(sum(2, 5)).toEqual(7);
+  });
+  it('should throw an error if either argument is not a number', () => {
+    expect(() => sum(1, 'a')).toThrow('Both arguments must be numbers.');
+    expect(() => sum('a', 1)).toThrow('Both arguments must be numbers.');
+  });
 });
