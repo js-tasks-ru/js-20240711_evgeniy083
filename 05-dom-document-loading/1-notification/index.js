fa05dbacd72e353235b61f3cc3bd306d05e058b7
Eugene Yakunichev <ev.iakunichev@stalcom.com>
2024-08-01 15:51:09 +0300

    Работа с DOM. Загрузка документа

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,35 @@
 export default class NotificationMessage {
 
+  constructor(message, { duration = 1000, type = 'success' } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    
+    this.element = this.createElement();
+  }
+    
+  createElement() {
+    const element = document.createElement('div');
+    element.classList.add('notification');
+    element.classList.add(this.type);
+    element.textContent = this.message;
+    
+    return element;
+  }
+    
+  show(container = document.body) {
+    container.append(this.element);
+    
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+    
+  remove() {
+    this.element.remove();
+  }
+    
+  destroy() {
+    this.remove();
+  }
 }
